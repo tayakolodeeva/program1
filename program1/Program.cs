ff04203ec5e0d35c9760bea57f1834fb4b6a0c78
Taya <tayakolodeeva@yandex.ru>
2022-08-31 23:52:36 +0300

    fix program

--- program1/Program.cs
+++ program1/Program.cs
@@ -1,11 +1,25 @@
 ﻿// Нахождение большего и меньшего из двух чисел
-{ Console.WriteLine("Введите 2 числа");
-int = numberOne, numberTwo;
-Console.WriteLine("Введите первое число");
-numberOne = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-numberTwo = Convert.ToInt32(Console.ReadLine());
-bool Chto0 = numberOne > numberTwo;
-bool Chto1 = a == b;
-bool Chto2 = a < b;
-f
+{ 
+    Console.WriteLine("Введите 2 числа");
+    int number, numberTwo;
+    Console.WriteLine("Введите первое число -");
+    number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число -");
+    numberTwo = Convert.ToInt32(Console.ReadLine());
+
+    bool suppose0 = number > numberTwo;
+    bool suppose1 = number == numberTwo;
+    bool suppose2 = number < numberTwo;
+
+  if (suppose0)
+{
+Console.WriteLine($"Число {number} больше чем {numberTwo}");
+}
+else if (suppose1)
+{
+Console.WriteLine($"Числа {number} и {numberTwo} равны");
+}
+else
+{
+Console.WriteLine($"Число {numberTwo} больше чем {number}");
+}}
