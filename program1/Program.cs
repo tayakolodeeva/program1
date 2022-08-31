0cdb40ffe3723e77335bd7e7addcde04d99aa5f1
Taya <tayakolodeeva@yandex.ru>
2022-08-31 22:52:29 +0300

    started program1

---
+++ program1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Нахождение большего и меньшего из двух чисел
+{ Console.WriteLine("Введите 2 числа");
+int = numberOne, numberTwo;
+Console.WriteLine("Введите первое число");
+numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numberTwo = Convert.ToInt32(Console.ReadLine());
+bool Chto0 = numberOne > numberTwo;
+bool Chto1 = a == b;
+bool Chto2 = a < b;
+f
