ee5c666c3b36d5ce50ddb53181883806159fdf7e
Taya <tayakolodeeva@yandex.ru>
2022-09-01 11:41:01 +0300

    fixprogram2

---
+++ program2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Нахождение максимального из трех двух чисел
+{ 
+    Console.WriteLine("Введите 3 числа");
+    int number, numberTwo, numberThree;
+    int max;
+    Console.WriteLine("Введите первое число -");
+    number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число -");
+    numberTwo = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число -");
+    numberThree = Convert.ToInt32(Console.ReadLine());
+    max = number;
+  if (max > numberTwo);
+    else
+{
+    max = numberTwo;
+}
+if (max > numberThree);
+   else
+{
+    max = numberThree;
+}
+{Console.WriteLine($"Максимальное {max}");
+}}
