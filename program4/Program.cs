3e309012af3cd43a2ce41465840b087e3f90a155
Taya <tayakolodeeva@yandex.ru>
2022-09-01 13:33:21 +0300

    fixprogram5

---
+++ program4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Все четные от 1 до N
+{ 
+    Console.WriteLine("Введите число");
+    int numberN;
+    Console.WriteLine("Введите число -");
+    numberN = Convert.ToInt32(Console.ReadLine());
+    for (int i=1; i < numberN; i++) 
+ {
+    if (i % 2 == 0) 
+{
+    Console.Write(i + " ");
+}
+}
+Console.ReadKey();
+}
+
