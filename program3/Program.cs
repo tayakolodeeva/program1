5769dba6588896a9313b67dcb9039d1efb4e42bc
Taya <tayakolodeeva@yandex.ru>
2022-09-01 12:17:06 +0300

    fixprogram3

---
+++ program3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли число чётным?
+{ 
+    Console.WriteLine("Введите число");
+    int numberN;
+    Console.WriteLine("Введите число -");
+    numberN = Convert.ToInt32(Console.ReadLine());
+    
+  if (numberN % 2==0)
+{
+Console.WriteLine($"Число четное {numberN}");
+}
+else
+{
+Console.WriteLine($"Число не является четным {numberN}");
+}}
